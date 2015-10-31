644d2dba46ce5bf2b4aa8fba90ee8ca92f5efada
Juho Kallio <juho.kallio@futurice.com>
2015-10-31 11:22:01 +0200

    Add basic lilypond to vexflow note conversion

--- web/javascript/converter.js
+++ web/javascript/converter.js
@@ -9,7 +9,12 @@ converter = {
 
     // Key is in Lilypond form, e.g. c, c'' or c,,
     convertKeyToVexflow: function convertKeyToVexflow(key) {
-        return key.charAt(0);
+        // Lilypond uses ' and , to notate different height of the same note
+        var ups = key.split("'").length - 1;
+        var downs = key.split(",").length - 1;
+        // c/4 matches Lilypond c
+        var height = 4 + ups - downs;
+        return key.charAt(0) + "/" + height;
     },
 
     // Duration is in Lilypond form, e.g. 16, 8 or 2
--- web/javascript/tests/converterTests.js
+++ web/javascript/tests/converterTests.js
@@ -3,5 +3,7 @@
  */
 
 QUnit.test("Note conversion test", function(assert) {
-  assert.equal(converter.convertKeyToVexflow("c"), "c/4", "Basic c note was converted correctly");
+  assert.equal(converter.convertKeyToVexflow("c"), "c/4", "Basic c note");
+  assert.equal(converter.convertKeyToVexflow("d'"), "d/5", "d note up one octave");
+  assert.equal(converter.convertKeyToVexflow("e,,"), "e/2", "e note down two octaves");
 });
