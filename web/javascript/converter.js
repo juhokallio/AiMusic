dc331ebbe68ca97751ab7157ac99bf6bb5b5c1c4
Juho Kallio <juho.kallio@futurice.com>
2015-10-31 11:55:19 +0200

    Add basic notation rendering

--- web/javascript/converter.js
+++ web/javascript/converter.js
@@ -1,14 +1,10 @@
-converter = {
-    // Note is of form ["c", "16], where the first cell is the key and second the duration
-    convert: function (note) {
-        return new Vex.Flow.StaveNote({
-                keys: [convertKeyToVexflow(note[0])],
-                duration: convertDurationToVexflow(note[1])
-            });
-    },
+/**
+ * Converter for converting Lilypond like notification used in the backend to renderable vexflow notation.
+ */
+converter = (function(){
 
     // Key is in Lilypond form, e.g. c, c'' or c,,
-    convertKey: function convertKeyToVexflow(key) {
+    function convertKey(key) {
         // For rests we define here only the visual position of the note
         if (key === "r") {
             return "b/4";
@@ -19,13 +15,23 @@ converter = {
         // c/4 matches Lilypond c
         var height = 4 + ups - downs;
         return key.charAt(0) + "/" + height;
-    },
+    }
 
     // Duration is in Lilypond form, e.g. 16, 8 or 2. We need here the key as well, since rests are marked in duration in vexflow
-    convertDuration: function (key, duration) {
+    function convertDuration(key, duration) {
         var suffix = key === "r" ? "r" : "";
         return duration + suffix;
     }
-};
 
-
+    return {
+        // Note is of form ["c", "16], where the first cell is the key and second the duration
+        convert: function (note) {
+            return new Vex.Flow.StaveNote({
+                    keys: [convertKey(note[0])],
+                    duration: convertDuration(note[0], note[1])
+                });
+        },
+        convertKey: convertKey,
+        convertDuration: convertDuration
+    };
+})();
--- web/javascript/training.js
+++ web/javascript/training.js
@@ -6,4 +6,25 @@ $(document).ready(function() {
     var ctx = renderer.getContext();
     var stave = new Vex.Flow.Stave(10, 0, 500);
     stave.addClef("treble").setContext(ctx).draw();
+
+    var notes = _.map(sampleMusic[0].notes, converter.convert);
+
+    // Create a voice in 4/4
+    var voice = new Vex.Flow.Voice({
+        num_beats: 4,
+        beat_value: 4,
+        resolution: Vex.Flow.RESOLUTION
+    });
+    // The API is kind of meh here, this has to be specified separately outside the initialization.
+    voice.setStrict(false);
+
+    // Add notes to voice
+    voice.addTickables(notes);
+
+    // Format and justify the notes to 500 pixels
+    var formatter = new Vex.Flow.Formatter().
+    joinVoices([voice]).format([voice], 500);
+
+    // Render voice
+    voice.draw(ctx, stave);
 });
